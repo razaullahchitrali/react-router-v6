2e273f07b91fd781f3dfe42aa52689430b67312f
razaullahchitrali <rrizvi340@gmail.com>
2024-02-26 07:52:25 +0500

    feat: udpate main branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import{
-  createBrowserRouter,
-  RouterProvider,
-}from "react-router-dom";
 import './index.css'
-import Root from './routes/root';
-import Error from './error';
-import Contact from './contact';
-
-const router =createBrowserRouter([
-  {
-    path:"/",
-    element:<Root/>,
-    errorElement:<Error/>
-  },
-  {
-    path:"/contact",
-    element:<Contact/>,
-  }
-])
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
+   <App/>
   </React.StrictMode>,
 )
